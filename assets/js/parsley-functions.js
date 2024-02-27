e954a5cbe659e84a92f1ef7565708b78abc5ab6a
Ronald Ponferrada <ronaldponferrada@Ronalds-MacBook-Air.local>
2024-02-26 20:54:40 -0800

    <feature: added table logic for confirmation page>

--- assets/js/parsley-functions.js
+++ assets/js/parsley-functions.js
@@ -1,41 +1,54 @@
 $(function () {
-    var $sections = $('.form-section');
-  
-    function navigateTo(index) {
-      // Mark the current section with the class 'current'
-      $sections
-        .removeClass('current')
-        .eq(index)
-          .addClass('current');
-      // Show only the navigation buttons that make sense for the current section:
-      $('.form-navigation .previous').toggle(index > 0);
-      var atTheEnd = index >= $sections.length - 1;
-      $('.form-navigation .next').toggle(!atTheEnd);
-      $('.form-navigation [type=submit]').toggle(atTheEnd);
-    }
-  
-    function curIndex() {
-      // Return the current index by looking at which section has the class 'current'
-      return $sections.index($sections.filter('.current'));
-    }
-  
-    // Previous button is easy, just go back
-    $('.form-navigation .previous').click(function() {
-      navigateTo(curIndex() - 1);
-    });
-  
-    // Next button goes forward if current block validates
-    $('.form-navigation .next').click(function() {
-      $('.form-registration').parsley().whenValidate({
-        group: 'block-' + curIndex()
-      }).done(function() {
-        navigateTo(curIndex() + 1);
-      });
-    });
-  
-    // Prepare sections by setting the `data-parsley-group` attribute to 'block-0', 'block-1', etc.
-    $sections.each(function(index, section) {
-      $(section).find(':input').attr('data-parsley-group', 'block-' + index);
-    });
-    navigateTo(0); // Start at the beginning
+  var $sections = $('.form-section');
+
+  function navigateTo(index) {
+    // Mark the current section with the class 'current'
+    $sections
+      .removeClass('current')
+      .eq(index)
+        .addClass('current');
+    // Show only the navigation buttons that make sense for the current section:
+    $('.form-navigation .previous').toggle(index > 0);
+    var atTheEnd = index >= $sections.length - 1;
+    $('.form-navigation .next').toggle(!atTheEnd);
+    $('.form-navigation [type=submit]').toggle(atTheEnd);
+  }
+
+  function curIndex() {
+    // Return the current index by looking at which section has the class 'current'
+    return $sections.index($sections.filter('.current'));
+  }
+
+  // Previous button is easy, just go back
+  $('.form-navigation .previous').click(function() {
+    navigateTo(curIndex() - 1);
   });
+
+  // Next button goes forward if current block validates
+  $('.form-navigation .next').click(function() {
+    $('.form-registration').parsley().whenValidate({
+      group: 'block-' + curIndex()
+    }).done(function() {
+      navigateTo(curIndex() + 1);
+    });
+  });
+
+  // Prepare sections by setting the `data-parsley-group` attribute to 'block-0', 'block-1', etc.
+  $sections.each(function(index, section) {
+    $(section).find(':input').attr('data-parsley-group', 'block-' + index);
+  });
+  navigateTo(0); // Start at the beginning
+});
+
+function displayResults() {
+  if ($('.form-section.confirmation-page').length > 0){
+    var firstName = $("#firstname").val();
+    var lastName = $("#lastname").val();
+    var emailAddy = $("#email").val();
+    $("td#fname").html(firstName);
+    $("td#lname").html(lastName);
+    $("td#email").html(emailAddy);
+  }
+}
+
+document.getElementById("nextBtn").addEventListener("click", displayResults);
